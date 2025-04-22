dd5fd8ce6639634409733825811e335adfbdcd14
LuckyVAL06 <lopezvalentinoscar@gmail.com>
2025-04-22 17:03:37 -0300

    cargar primer DropDownList

--- TP4-Grupo_8/ejercicio1.aspx.cs
+++ TP4-Grupo_8/ejercicio1.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace TP4_Grupo_8
 {
@@ -12,6 +13,7 @@ namespace TP4_Grupo_8
     {
         //private const string cadenaConexion = @" Data Source = DESKTOP - CCJO3LV\SQLEXPRESS; Initial Catalog = Neptuno; Integrated Security = True";
         string cadenaConexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Viajes;Integrated Security = True";
+        private string consultaSQL = "SELECT * FROM Provincias SELECT * FROM Localidades";
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -19,8 +21,15 @@ namespace TP4_Grupo_8
             {
                 SqlConnection connection = new SqlConnection(cadenaConexion);
                 connection.Open();
-            }
 
+                SqlCommand sqlCommand = new SqlCommand(consultaSQL, connection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                ddlProvinciaInicio.DataSource = sqlDataReader;
+                ddlProvinciaInicio.DataTextField = "NombreProvincia";
+                ddlProvinciaInicio.DataValueField = "IdProvincia";
+                ddlProvinciaInicio.DataBind();
+            }
         }
     }
 }
