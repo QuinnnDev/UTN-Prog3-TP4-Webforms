74a422f3955ad9bcb1d8358445f72d4be19508ec
Xhispas <acevedo.candela066@gmail.com>
2025-04-22 14:28:58 -0300

    CONEXION CON LA BASE DE DATOS

---
+++ TP4-Grupo_8/ejercicio1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace TP4_Grupo_8
+{
+    public partial class ejercicio1 : System.Web.UI.Page
+    {
+        private const string cadenaConexion = @" Data Source = DESKTOP - CCJO3LV\SQLEXPRESS; Initial Catalog = Neptuno; Integrated Security = True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                SqlConnection connection = new SqlConnection(cadenaConexion);
+                connection.Open();
+            }
+
+        }
+    }
+}
