14ab3d58333f0a2f9adc39de7701596113398217
IgnacioCalva <ignaciocalva15@gmail.com>
2025-04-24 13:29:36 -0300

    Ejercicio 2 / Conexion con DB

---
+++ TP4-Grupo_8/ejercicio2.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP4_Grupo_8
+{
+    public partial class ejercicio2 : System.Web.UI.Page
+    {
+        string conexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Neptuno;Integrated Security = True;Encrypt=False";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(conexion);
+            connection.Open();
+        }
+    }
+}
