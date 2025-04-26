3cc582c3d1503a381779dc06635750edb6f33862
Xhispas <acevedo.candela066@gmail.com>
2025-04-26 17:31:44 -0300

    conexion y diseño jijijuju
    callate val

--- TP4-Grupo_8/ListadoLibros.aspx.designer.cs
+++ TP4-Grupo_8/ListadoLibros.aspx.designer.cs
@@ -24,6 +24,33 @@ namespace TP4_Grupo_8
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control lblListadoDeLibros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblListadoDeLibros;
+
+        /// <summary>
+        /// Control gvLibros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvLibros;
+
+        /// <summary>
+        /// Control linkVerTemas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton linkConsultar;
+
+        /// <summary>
         /// Control lblTestVariable.
         /// </summary>
         /// <remarks>
