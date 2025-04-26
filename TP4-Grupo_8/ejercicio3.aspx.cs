4fa90f2a1e65181a3f1d0ca2ca8babcfbc500542
IgnacioCalva <ignaciocalva15@gmail.com>
2025-04-25 22:29:53 -0300

    Ej3 - Conexion con BD

---
+++ TP4-Grupo_8/ejercicio3.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP4_Grupo_8
+{
+    public partial class ejercicio3 : System.Web.UI.Page
+    {
+        string conexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Libreria;Integrated Security = True;Encrypt=False";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack) 
+            {
+                SqlConnection connection = new SqlConnection(conexion);
+                connection.Open();
+
+
+                connection.Close();
+            }
+        }
+    }
+}
