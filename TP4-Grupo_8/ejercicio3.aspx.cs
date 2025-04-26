7bd58ed32800307dce716d89e8e9e6f018f60923
LuckyVAL06 <lopezvalentinoscar@gmail.com>
2025-04-26 14:40:16 -0300

    cargar DDL

--- TP4-Grupo_8/ejercicio3.aspx.cs
+++ TP4-Grupo_8/ejercicio3.aspx.cs
@@ -10,8 +10,8 @@ namespace TP4_Grupo_8
 {
     public partial class ejercicio3 : System.Web.UI.Page
     {
-        string conexion = "Data Source=localhost\\sqlexpress;Initial Catalog=Libreria;Integrated Security = True;Encrypt=False";
-        string consultaSQL = "SELECT * FROM Libros";
+        string conexion = "Data Source=localhost\\sqlexpress01;Initial Catalog=Libreria;Integrated Security = True;Encrypt=False";
+        string consultaSQL = "SELECT IdTema FROM Libros GROUP BY IdTema";
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack) 
@@ -22,6 +22,18 @@ namespace TP4_Grupo_8
                 SqlCommand cmd = new SqlCommand(consultaSQL, connection);
                 SqlDataReader reader = cmd.ExecuteReader();
 
+                List<ListItem> items = new List<ListItem>();
+
+                while (reader.Read())
+                {
+                    string idTema = reader["IdTema"].ToString();
+                    items.Add(new ListItem("Tema " + idTema, idTema));
+                }
+
+                ddlTemas.DataSource = items;
+                ddlTemas.DataTextField = "Text";
+                ddlTemas.DataValueField = "Value";
+                ddlTemas.DataBind();
 
                 connection.Close();
             }
